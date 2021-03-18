81a61d8f7194598d8df4a87a407ccee7d5c50433
maddeer-timer <1044734463@qq.com>
2021-03-18 14:49:19 +0800

    Backup 2021.3.18

--- Panels/Control.py
+++ Panels/Control.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import os
 import sys
+import traceback
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QApplication,QMainWindow,QFileDialog
 import Panels.Gui as Gui
@@ -8,7 +9,9 @@ import Core
 # 创建函数
 def create():
     # 重要标志和变量
-    pass
+    global MenuChecked,StatusChecked
+    MenuChecked=False
+    StatusChecked=True
 # 销毁函数
 def destroy():
     pass
@@ -19,10 +22,42 @@ class MyMainWindow(QMainWindow):
         super(MyMainWindow,self).__init__()
         pass
     def closeEvent(self,event):
+        super(MyMainWindow,self).closeEvent(event)
         destroy()
         sys.exit(0)
+    # 处理函数
+    def action_open(self,action):
+        pass
+    def action_recent(self, action):
+        pass
+    def action_export(self,action):
+        pass
+    def action_export_all(self,action):
+        pass
+    def action_save_image(self,action):
+        pass
+    def action_close(self,action):
+        pass
+    def action_close_all(self,action):
+        pass
+    def action_exit(self,action):
+        pass
+    def action_menu(self,action):
+        pass
+    def action_status(self,action):
+        pass
+    def action_help(self,action):
+        pass
+    def action_about(self,action):
+        pass
     # 输出槽(信号)
-    pass
+    def triggered(self,action):
+        # 直接执行对应的函数代码
+        try:
+            eval("self.{}(action)".format(action.objectName()))
+        except:
+            traceback.print_exc()
+            self.close()
 # 主函数
 def main():
     create()
