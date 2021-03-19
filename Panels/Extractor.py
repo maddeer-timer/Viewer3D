f12f88932e73b5570ded51f2cec53efff2c26999
maddeer-timer <1044734463@qq.com>
2021-03-19 16:53:11 +0800

    Backup 2021.3.19

--- Panels/Extractor.py
+++ Panels/Extractor.py
@@ -5,56 +5,118 @@ import sys
 import re
 import subprocess
 import traceback
+def getRelativePath(Workpath,Filepath):
+    """
+    用来获取相对路径的工具
+    返回从Workpath到Filepath的相对路径
+    """
+    
 def extractor(FilepathList,TargetPath):
     """
     实用工具Extractor\n
     用于将Python文件打包进一个C++文件并调用LUpdate转化成ts文件\n
     做出该文件的原因是因为PyQT5的PyLUpdate无法正常使用\n
     """
-    for Filepath in FilepathList:
+    FilesContent=""
+    for FileIndex,Filepath in enumerate(FilepathList):
         try:
             # 读取Python文件
-            if os.path.splitext(Filepath)[1]!=".py":
+            if os.path.splitext(Filepath)[1].lower()!=".py":
                 raise ValueError("\"{}\" isn't a Python file".format(Filepath))
-            FilesContent=""
             with open(Filepath,"r",encoding="utf-8") as File:
                 ThisContent=File.read()
                 TrFuncList=re.findall(r'_translate\(\".*?\",\s*?\".*?\"\)',ThisContent)
-                FilesContent+="\n".join([TrFunc.replace("\n","").replace("_translate","\tObject::tr")
-                                         for TrFunc in TrFuncList])
-            # 合并入C++文件
-            TemporaryFile=os.path.splitext(TargetPath)[0]+".cpp"
-            with open(TemporaryFile,"w",encoding="utf-8") as File:
-                File.write(FilesContent)
-            # 执行LUpdate并输出日志
-            print("LUpdate执行日志: ")
-            LUpdatePath="\"D:\\Visual Studio\\Microsoft Visual Studio\\Shared\\Python37_64\\Lib"\
-                        "\\site-packages\\qt5_applications\\Qt\\bin\\lupdate.exe\""
-            ShellResult=subprocess.Popen(r"{exec} {temp} -ts {ts}".format(
-                exec=LUpdatePath,temp=TemporaryFile,ts=TargetPath),
-                stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-            stdout,stderr=ShellResult.communicate()
-            print(stderr.decode("gbk"),end="")
-            print(stdout.decode("gbk"),end="")
-            # 删除临时C++文件
-            os.remove(TemporaryFile)
+                CppTrFuncList=[re.sub(r'"\s*\)','\a{}")'.format(str(FileIndex)),
+                                      TrFunc.replace("\n","").replace("_translate","Object::tr"))
+                               for TrFunc in TrFuncList]
+                FilesContent+="\n".join(CppTrFuncList)
         except:
             traceback.print_exc()
-            if "TemporaryFile" in locals() and os.path.exists(TemporaryFile):
-                os.remove(TemporaryFile)
             break
+    try:
+        # 合并入C++文件
+        TemporaryFile=os.path.split(TargetPath)[0]+"_translate.bak"
+        with open(TemporaryFile,"w",encoding="utf-8") as File:
+            File.write(FilesContent)
+        # 执行LUpdate并输出日志
+        print("LUpdate执行日志: ")
+        LUpdatePath="\"D:\\Visual Studio\\Microsoft Visual Studio\\Shared\\Python37_64\\Lib"\
+                    "\\site-packages\\qt5_applications\\Qt\\bin\\lupdate.exe\""
+        ShellResult=subprocess.Popen(r"{exec} {temp} -ts {ts}".format(
+            exec=LUpdatePath,temp=TemporaryFile,ts=TargetPath),
+            stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        stdout,stderr=ShellResult.communicate()
+        print(stderr.decode("gbk"),end="")
+        print(stdout.decode("gbk"),end="")
+        # 删除临时C++文件
+        os.remove(TemporaryFile)
+        # 修改ts文件使之与源文件相对应
+        with open(TargetPath,"r",encoding="utf-8") as File:
+            TsFileContent=File.readlines()
+        BeginIndex=-1
+        FileIndex=-1
+        SourceName=""
+        CommentName=""
+        ReLocation=re.compile(r'\s*<location filename="(.*?)" line="(\d*?)"/>')
+        ReSource=re.compile(r'\s*<source>(.*?)</source>')
+        ReComment=re.compile(r'\s*<comment>(.*?)<byte value="x7"/>(\d*?)</comment>')
+        for XmlLineIndex,XmlLine in enumerate(TsFileContent):
+            if re.match(r"\s*<message>",XmlLine) is not None:
+                BeginIndex=XmlLineIndex
+                continue
+            if ReSource.match(XmlLine) is not None:
+                SourceName=ReSource.findall(XmlLine)[0]
+                continue
+            if ReComment.match(XmlLine) is not None:
+                CommentName,FileIndex=ReComment.findall(XmlLine)[0]
+                FileIndex=int(FileIndex)
+                continue
+            if re.match(r"\s*</message>",XmlLine) is not None:
+                # 获取该字符串原来所在的位置列表
+                Filepath=FilepathList[FileIndex]
+                LocationList=[]
+                with open(Filepath,"r",encoding="utf-8") as File:
+                    FileLineList=File.readlines()
+                for FileLineIndex,FileLine in FileLineList:
+                    if re.search(r'_translate\(\"{src}\",\s*?\"{cmt}\"\)'.format(
+                            src=SourceName,cmt=CommentName),FileLine):
+                        LocationList.append(FileLineIndex+1)
+                # 对ts文件的内容进行修正
+                LocationCounter=0
+                for SubXmlLineIndex in range(BeginIndex+1,XmlLineIndex):
+                    XmlLineContent=TsFileContent[SubXmlLineIndex]
+                    if ReLocation.match(XmlLineContent) is not None:
+                        TsFileContent[SubXmlLineIndex]=re.sub(
+                            r'filename="(.*?)"',r'filename="{}"'.format(getRelativePath(
+                                os.path.abspath(os.getcwd()),os.path.abspath(Filepath))),
+                            XmlLineContent)
+                        TsFileContent[SubXmlLineIndex]=re.sub(
+                            r'line="(\d*?)"',r'line="{}"'.format(
+                                LocationList[LocationCounter]),XmlLineContent)
+                        LocationCounter+=1
+                        continue
+                    if ReComment.match(XmlLineContent) is not None:
+                        TsFileContent[SubXmlLineIndex]=re.sub(
+                            r'',r'',XmlLineContent)
+                        continue
+                continue
+    except:
+        traceback.print_exc()
+        if "TemporaryFile" in locals() and os.path.exists(TemporaryFile):
+            os.remove(TemporaryFile)
+        sys.exit()
 if __name__=="__main__":
     try:
         print("请输入Python文件路径列表(使用\"Exit\"表示列表的结束): ")
         FilepathList=[]
         while True:
             Filepath=input()
-            if Filepath=="Exit": break
+            if Filepath.lower()=="exit": break
             FilepathList.append(Filepath)
         if len(FilepathList)==0:
             raise ValueError("The file list is empty")
         TargetPath=input("请输入目标文件路径(必须是ts文件): \n")
-        if os.path.splitext(TargetPath)[1]!=".ts":
+        if os.path.splitext(TargetPath)[1].lower()!=".ts":
             raise ValueError("The target file isn't a ts file")
         extractor(FilepathList, TargetPath)
     except:
---
+++ Panels/MainWindow.ts
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Object</name>
+    <message>
+        <location filename="_translate.bak" line="1"/>
+        <source>MainWindow</source>
+        <comment>Viewer3D<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="2"/>
+        <source>MainWindow</source>
+        <comment>文件(&amp;F)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="3"/>
+        <location filename="_translate.bak" line="20"/>
+        <source>MainWindow</source>
+        <comment>帮助(&amp;H)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="4"/>
+        <source>MainWindow</source>
+        <comment>视图(&amp;V)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="5"/>
+        <source>MainWindow</source>
+        <comment>关于(&amp;A)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="6"/>
+        <source>MainWindow</source>
+        <comment>显示有关Viewer3D的信息<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="7"/>
+        <source>MainWindow</source>
+        <comment>打开(&amp;O)...<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="8"/>
+        <source>MainWindow</source>
+        <comment>打开3D模型文件<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="9"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+O<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="10"/>
+        <source>MainWindow</source>
+        <comment>保存图像(&amp;I)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="11"/>
+        <source>MainWindow</source>
+        <comment>保存当前视图<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="12"/>
+        <source>MainWindow</source>
+        <comment>关闭文件(&amp;C)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="13"/>
+        <source>MainWindow</source>
+        <comment>关闭3D模型文件<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="14"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+C<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="15"/>
+        <source>MainWindow</source>
+        <comment>退出(&amp;X)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="16"/>
+        <source>MainWindow</source>
+        <comment>退出应用程序<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="17"/>
+        <source>MainWindow</source>
+        <comment>关闭全部(&amp;L)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="18"/>
+        <source>MainWindow</source>
+        <comment>关闭全部文件<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="19"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+Shift+C<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="21"/>
+        <source>MainWindow</source>
+        <comment>显示帮助内容<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="22"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+H<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="23"/>
+        <source>MainWindow</source>
+        <comment>导出全部(&amp;A)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="24"/>
+        <source>MainWindow</source>
+        <comment>导出全部文件<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="25"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+Shift+E<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="26"/>
+        <source>MainWindow</source>
+        <comment>文件菜单(&amp;F)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="27"/>
+        <source>MainWindow</source>
+        <comment>显示/隐藏文件菜单<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="28"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+F<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="29"/>
+        <source>MainWindow</source>
+        <comment>状态栏(&amp;S)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="30"/>
+        <source>MainWindow</source>
+        <comment>显示/隐藏状态栏<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="31"/>
+        <source>MainWindow</source>
+        <comment>导出(&amp;E)<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="32"/>
+        <source>MainWindow</source>
+        <comment>导出3D模型文件<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="_translate.bak" line="33"/>
+        <source>MainWindow</source>
+        <comment>Ctrl+E<byte value="x7"/>0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
