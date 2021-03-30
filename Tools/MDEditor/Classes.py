98436b0faa0249bd39e94dfc73ee61e1e804ba82
maddeer-timer <1044734463@qq.com>
2021-03-31 03:03:04 +0800

    Backup 2021.3.31

--- Tools/MDEditor/Classes.py
+++ Tools/MDEditor/Classes.py
@@ -1,9 +1,24 @@
 # coding=utf-8
+from PyQt5 import QtCore,QtGui,QtWidgets
 from PyQt5 import QtWebEngineWidgets
 from PyQt5 import Qsci
+# 实用工具类MyDocument, 用来接收并存储编辑器的值
+class MyDocument(QtCore.QObject):
+    textChanged=QtCore.pyqtSignal(str)
+    def __init__(self,Parent=None):
+        super(MyDocument,self).__init__(Parent)
+        self.Text=""
+    def setText(self,Text):
+        if Text==self.Text: return
+        self.Text=Text
+        self.textChanged.emit(self.Text)
 # 重载QsciLexerMarkdown
 class MyLexerMarkdown(Qsci.QsciLexerMarkdown):
     pass
-# 重载QWebEnginePage, 用来显示预览
+# 重载QWebEnginePage, 用来阻止页面切换
 class MyWebEnginePage(QtWebEngineWidgets.QWebEnginePage):
-    pass
+    def acceptNavigationRequest(self,Url,Type,IsMainFrame):
+        # 只允许加载 qrc:/index.html
+        if Url.scheme()=="qrc": return True
+        QtGui.QDesktopServices.openUrl(Url)
+        return False
--- Tools/MDEditor/Gui.py
+++ Tools/MDEditor/Gui.py
@@ -4,19 +4,35 @@ from MainWindow import *
 from Classes import *
 # 重载Ui_MainWindow类
 class MyUi_MainWindow(Ui_MainWindow):
+    # 初始化
+    def __init__(self):
+        super(MyUi_MainWindow,self).__init__()
+        self.Content=MyDocument()
     def setupUi(self,MainWindow):
         super(MyUi_MainWindow,self).setupUi(MainWindow)
-        # 对Editor(使用QsciScintilla)进行初始化
-
+        # 对Editor(使用QsciScintilla)进行设置
+        self.sciScintilla.setUtf8(True)
+        LexerMarkdown=MyLexerMarkdown(self.sciScintilla)
+        self.sciScintilla.setLexer(LexerMarkdown)
         # 对Preview(使用QWebEngineView)进行初始化
         WebEnginePage=MyWebEnginePage(MainWindow)
         self.webEngineView.setPage(WebEnginePage)
+        self.sciScintilla.textChanged.connect(lambda:self.Content.setText(
+            self.sciScintilla.text()))
+        WebChannel=QtWebChannel.QWebChannel(MainWindow)
+        WebChannel.registerObject("content",self.Content)
+        WebEnginePage.setWebChannel(WebChannel)
         self.webEngineView.setUrl(QtCore.QUrl("qrc:/Resources/Index.html"))
         # 设置Editor和Preview的显示与否
         self.displayEditor(True)
         self.displayPreview(False)
+        # 对Editor(使用QsciScintilla)进行初始化
+        defaultTextFile=QtCore.QFile(":/Resources/Default.md")
+        defaultTextFile.open(QtCore.QIODevice.ReadOnly)
+        self.sciScintilla.setText(defaultTextFile.readAll().data().decode("UTF-8"))
         # 连接信号和槽
         MainWindow.retranslateUi.connect(self.retranslateUi)
+    # 处理函数
     def displayEditor(self,Visible):
         pass
     def displayPreview(self,Visible):
