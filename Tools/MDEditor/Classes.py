476b3075fe496cc79db006e00f1788e83ef548aa
maddeer-timer <1044734463@qq.com>
2021-03-31 05:13:29 +0800

    Backup 2021.3.31

--- Tools/MDEditor/Classes.py
+++ Tools/MDEditor/Classes.py
@@ -1,17 +1,25 @@
 # coding=utf-8
 from PyQt5 import QtCore,QtGui,QtWidgets
+from PyQt5.QtCore import pyqtProperty
 from PyQt5 import QtWebEngineWidgets
 from PyQt5 import Qsci
 # 实用工具类MyDocument, 用来接收并存储编辑器的值
 class MyDocument(QtCore.QObject):
+    # 信号及宏定义
     textChanged=QtCore.pyqtSignal(str)
+    # 类属性定义
+    @pyqtProperty(str)
+    def text(self):
+        return self._text
+    # 初始化
     def __init__(self,Parent=None):
         super(MyDocument,self).__init__(Parent)
-        self.Text=""
+        self._text=""
+    # 槽函数定义
     def setText(self,Text):
-        if Text==self.Text: return
-        self.Text=Text
-        self.textChanged.emit(self.Text)
+        if Text==self._text: return
+        self._text=Text
+        self.textChanged.emit(self._text)
 # 重载QsciLexerMarkdown
 class MyLexerMarkdown(Qsci.QsciLexerMarkdown):
     pass
