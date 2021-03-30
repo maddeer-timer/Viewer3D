3660902403aae8f980577124026b50688a90f343
maddeer-timer <1044734463@qq.com>
2021-03-30 19:54:32 +0800

    Backup 2021.3.30

--- Tools/MDEditor/Classes.py
+++ Tools/MDEditor/Classes.py
@@ -1,4 +1,5 @@
 # coding=utf-8
 from PyQt5 import QtCore,QtGui,QtWidgets
-from PyQt5 import QtWebChannel
+from PyQt5 import QtWebChannel,QtWebEngineWidgets
+from PyQt5 import Qsci
 # 重载QWebEnginePage
