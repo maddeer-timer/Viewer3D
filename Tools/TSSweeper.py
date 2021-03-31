077d9e51f8e94bbe76427ae115155f2f65595174
maddeer-timer <1044734463@qq.com>
2021-04-01 03:55:51 +0800

    Backup 2021.4.1

--- Tools/TSSweeper.py
+++ Tools/TSSweeper.py
@@ -12,26 +12,48 @@ def TSSweeper(Filepath):
     """
     try:
         # 读取TS文件
+        print("正在读取ts文件...",end="",flush=True)
         if len(Filepath)==0 or os.path.splitext(Filepath)[1].lower()!=".ts":
             raise ValueError("\"{}\"不是ts文件".format(Filepath))
         ThisContent=""
         with open(Filepath,"r",encoding="utf-8") as File:
             ThisContent=File.read()
+        print("done",flush=True)
         # 处理文件内容
-        TranslatePattern=re.compile(r'<translation>.*?</translation>',flags=re.DOTALL)
+        print("正在查找并替换文件内容...",end="",flush=True)
+        TranslatePattern=re.compile(r'<translation\s*(type="(?P<type>.*?)")?>'
+                                    r'(?P<content>.*?)</translation>',flags=re.DOTALL)
         ReplaceString=r'<translation type="unfinished"></translation>'
         ReplaceStringLength=len(ReplaceString)
+        MatchObjectCounter={}
         MatchObject=TranslatePattern.search(ThisContent)
         while MatchObject is not None:
-            ThisContent=ThisContent.replace(MatchObject.group(),ReplaceString)
+            TranslationType=MatchObject.group("type")
+            if TranslationType is None: TranslationType="NoneType"
+            Addend=len(MatchObject.group("content"))!=0
+            if TranslationType not in MatchObjectCounter:
+                MatchObjectCounter[TranslationType]=[0,0]
+            MatchObjectCounter[TranslationType][Addend]+=1
+            ThisContent=ThisContent.replace(MatchObject.group(),ReplaceString,1)
             MatchObject=TranslatePattern.search(
                 ThisContent,MatchObject.start()+ReplaceStringLength)
+        print("done",flush=True)
+        print("查找\"{}\"...".format(Filepath),flush=True)
+        TranslationTotal=0
+        for Type,(Default,Translation) in MatchObjectCounter.items():
+            Total=Default+Translation
+            print("\t发现类型\"{Type}\"的标签共{Total}个: 其中{Default}个为空, "
+                  "{Translation}个已翻译".format(Type=Type,Total=
+            Total,Default=Default,Translation=Translation),flush=True)
+            Translation+=Total
         # 改名原文件写入新文件
+        print("正在写入\"{}\"...".format(Filepath),end="",flush=True)
         SplitextList=os.path.splitext(Filepath)
         BackupFilepath=SplitextList[0]+"_Bak"+SplitextList[1]
         os.rename(Filepath,BackupFilepath)
         with open(Filepath,"w",encoding="utf-8") as File:
             File.write(ThisContent)
+        print("done",flush=True)
     except:
         print()
         traceback.print_exc()
